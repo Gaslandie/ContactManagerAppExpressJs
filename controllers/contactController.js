55c7ebecbd16ae284db6cda5823d4b7a3fad91e4
Gassama <gassma803@gmail.com>
2024-02-25 23:31:39 +0100

    debut users

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,25 +5,26 @@
 //  chaque fois. express-async-handler simplifie ce processus en encapsulant automatiquement les routes asynchrones dans une fonction 
 // qui gère les erreurs. Ainsi, tu peux écrire des routes asynchrones de manière plus concise.
 const asyncHandler = require('express-async-handler');
-const contact = require('../models/contactModel');
+const Contact = require('../models/contactModel');
 //desc Get contacts
 //@route Get /api/contacts
 //@access public
 getContacts =asyncHandler(async(req,res) => {
-    const contacts = await contact.find({});
+    const contacts = await Contact.find({});
     res.status(200).json(contacts);
 });
 
 //desc Get a contact by id
 //@route Get /api/contacts/:id
 //@access public
-getContact = asyncHandler((req,res) => {
-    try {
-        const {id} = req.params;
-        res.status(200).json({mess:`there is your contact with id:${id}`});
-    } catch (error) {
-        res.send(500).json({mess: error.message});
-    } 
+getContact = asyncHandler(async (req,res) => {
+    const { id } = req.params;
+    const contact = await Contact.findById(id);
+    if(!contact){
+        res.status(404);
+        throw new Error("contact not found");
+    }
+    res.status(200).json(contact);
 });
 
 //desc Create contact
@@ -35,7 +36,7 @@ createContact = asyncHandler(async(req,res) => {
         res.status(400);
         throw new Error("all fields are mandatory");
     }
-    const contact = await contact.create({name,email,phone});
+    const contact = await Contact.create({name,email,phone});
     res.status(201).json(contact);
 });
 
@@ -43,24 +44,35 @@ createContact = asyncHandler(async(req,res) => {
 //@route Put /api/contacts/:id
 //@access public
 updateContact = asyncHandler(async(req,res) => {
-    try {
+   
         const { id } = req.params;
-        res.status(200).json({mess:`product updated with id:${id}`});
-    } catch (error) {
-        res.send(500).json({mess: error.message});
-    } 
+        const {name,email,phone} = req.body;
+        const contact = await Contact.findById(id)
+        if(!contact){
+            res.status(404);
+            throw new Error("contact not found");
+        }else{
+            contact.name = name || contact.name;
+            contact.email = email || contact.email;
+            contact.phone = phone || contact.phone;
+            await contact.save();
+        }
+        res.status(200).json({mess:`contact updated with id:${id}`});
 });
 
 //desc Delete contact
 //@route Delete /api/contacts/:id
 //@access public
 deleteContact = asyncHandler(async(req,res) => {
-    try {
         const { id } = req.params;
-        res.status(200).json({mess:`product with id:${id} deleted`});
-    } catch (error) {
-        res.send(500).json({mess: error.message});
-    } 
+        const contact = await Contact.findById(id);
+        if(!contact){
+            res.status(404);
+            throw new Error("contact not found");
+        }else{
+            await contact.remove();
+        }
+        res.status(200).json({mess:`contact with id:${id} deleted`});
 })
 
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const contactRoutes = require('./routes/contactRoutes');
+const userRoutes = require('./routes/userRoutes');
 const errorHandler = require('./middlewares/errorHandler');
 const connectDB = require('./config/dbConnection');
 
@@ -12,6 +13,7 @@ app.use(express.json())
 
 //routes
 app.use('/api/contacts',contactRoutes);
+app.use('/api/users',userRoutes);
 
 //global error handler
 app.use(errorHandler);
